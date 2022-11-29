5a7fe1b1e38a33212b466053d8c4aa521fbc930a
Jay Nayak <80544430+jayknayak@users.noreply.github.com>
2022-11-29 11:28:38 -0800

    Update routing.py

--- websocket/routing.py
+++ websocket/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from websocket import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/events/$', consumers.WebsocketConsumer),
+    url(r'^ws/message/$', consumers.WebsocketConsumer),
 ]
